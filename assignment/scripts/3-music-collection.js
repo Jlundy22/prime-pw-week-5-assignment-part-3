25f7cec60f11940551462cec0cf89a5ef19edd0b
Jack Lund <jacklund920@gmail.com>
2022-03-23 11:30:52 -0500

    added search funtion, still need to find a solution if there is an empty search object or no search object at all

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,6 @@
 console.log('***** Music Collection *****')
 let collection = [];
-// Factory funstion to add an object of album info to the collection array
+// Factory function to add an object of album info to the collection array
 const addTocollection = (albumTitle, artist, yearPublished) => {
     if (albumTitle && artist && yearPublished) {
         albumTitle =albumTitle.toUpperCase();
@@ -16,6 +16,7 @@ const addTocollection = (albumTitle, artist, yearPublished) => {
         return 'Need more info. Check to make sure you have an album title, artist, and year published.'
     }
 }
+// adding albums to collection array
 console.log(addTocollection('Bloom','Rufus Du Sol', 2016));
 console.log(addTocollection('American Candy','The Maine', 2015));
 console.log(addTocollection('Solace','Rufus Du Sol', 2018));
@@ -32,17 +33,72 @@ const showCollection = array => {
 
 }
 showCollection(collection);
-//console.log(addTocollection('Bloom', 2016));
+//Test if addToCollection does not have enough info
+console.log(addTocollection('Bloom', 2016));
 
+//function for find all albums by an artist in the collection array
 const findByArtist = artistSearch => {
     artistSearch = artistSearch.toUpperCase();
     let artistArray = [];
     for (info of collection) {
         if (info.artist === artistSearch) {
             artistArray.push(info);
-        }
+        } 
     } return artistArray
 }
+
+//testing findByArtist function
  console.log(findByArtist('rufus du sol'));
  console.log(findByArtist('junGle'));
  console.log(findByArtist('arcade fire'));
+ 
+ const search = searchObject => {
+    searchObject.artist= searchObject.artist.toUpperCase();
+    let artistSearchByYear =[];
+    for (artistYear of collection){
+        //console.log(objectToSearch.year)
+        //console.log(artistYear.artist);
+        //console.log(artistYear.yearPublished);
+        //console.log(objectToSearch.artist)
+       if (artistYear.artist === searchObject.artist && artistYear.yearPublished === searchObject.year) {
+           artistSearchByYear.push(artistYear)
+       }
+    } return artistSearchByYear
+   }
+   console.log(search({artist:'the maine', year: 2015}));
+
+ 
+ 
+ 
+ 
+ 
+ /*const search = searchObject => {
+     let objectToSearch = searchObject
+     objectToSearch.artist= objectToSearch.artist.toUpperCase();
+     let artistSearchByYear =[];
+     for (artistYear of collection){
+         //console.log(objectToSearch.year)
+         //console.log(artistYear.artist);
+         //console.log(artistYear.yearPublished);
+         //console.log(objectToSearch.artist)
+        if (artistYear.artist === objectToSearch.artist && artistYear.yearPublished === objectToSearch.year) {
+            artistSearchByYear.push(artistYear)
+        }
+     } return artistSearchByYear
+    }
+    console.log(search({artist:'the maine' , year: 2015  }));
+ 
+ */
+ 
+ /*const search = (artist, year) => {
+     artist = artist.toUpperCase();
+     let artistSearchByYear = [];
+     for (artistYear of collection){
+         if (artistYear.artist === artist && artistYear.yearPublished === year) {
+             artistSearchByYear.push({artistYear})
+         }
+       } return artistSearchByYear;
+       
+     }
+     console.log(search('jungle', 2018));
+     */
